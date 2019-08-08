99364254bbb15a79d0df9b7f5e82b7cc777d4faa
kennethmokhethi <kennethmahlatsemokhethi@gmail.com>
2019-08-08 13:40:32 +0200

    Implemented the function that multiply two numbers togther on the multiplyTwoNumbers.js file=>successful

---
+++ spec/multiplyNumbersSpec.js
@@ -0,0 +1,12 @@
+// multiple numbers spec
+describe("multiple numbers",()=>{
+   it("multiple two numbers",()=>{
+      var num1=1;
+      var num2=2;
+      var product=multiplyTwoNum(num1,num2);
+      expect(product).toBe(2);
+
+   });
+
+
+});
