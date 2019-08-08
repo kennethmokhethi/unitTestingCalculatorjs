8d1c6422aeaad3984c616c6d185296ebac4a19df
kennethmokhethi <kennethmahlatsemokhethi@gmail.com>
2019-08-08 11:22:15 +0200

    Deleted the redandunt files and added the addTwo,addMultiple,multipleTwo and multiplyMultiplenumbers functions

--- spec/evenNumSpec.js
+++
@@ -1,8 +0,0 @@
-describe("Even and odd numbers", () => {
-    it("Return even nummbers", () => {
-        var arr = [2, 4, 1, 3, 7, 8, 9];
-        var ans = findEven(arr);
-
-        expect(ans).toBe(2, 4, 8);
-    });
-});
